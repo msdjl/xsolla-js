a91c1dbe802f6e0a241cebc55fcbe83c158d60a1
msdjl <msdjl@mail.ru>
2016-02-29 11:48:30 +0500

    tests are updated

--- test/promotions.js
+++ test/promotions.js
@@ -10,55 +10,102 @@ const chakram = require('chakram'),
     co = require('co');
 
 describe('Xsolla API', function () {
+    before('define responses', function () {
+        chakram.addMethod('errorResponse', function (res, code) {
+            expect(res)
+                .to.have.schema(schemas.error)
+                .to.have.status(code)
+                .to.have.json('http_status_code', code);
+        });
+    });
     describe('Promotions', function () {
-        describe('Create/Get', function () {
+        before('define responses', function () {
+            chakram.addProperty('createPromotionResponse', function (res) {
+                expect(res)
+                    .to.have.status(201)
+                    .to.have.schema(schemas.createPromotionResponse);
+            });
+            chakram.addProperty('getPromotionResponse', function (res) {
+                expect(res)
+                    .to.have.status(200)
+                    .to.have.schema(schemas.getPromotionResponse);
+            });
+            chakram.addProperty('getAllPromotionsResponse', function (res) {
+                expect(res)
+                    .to.have.status(200)
+                    .to.have.schema(schemas.getAllPromotionsResponse);
+            });
+            chakram.addProperty('deletePromotionResponse', function (res) {
+                expect(res).to.have.status(204);
+                expect(res.body).to.be.empty;
+            });
+        });
+        describe('Create promotion', function () {
             dd(testData.correctPromotionsOpts, function () {
-                it('should create/get promotion when {description}', co.wrap(function* (promotion){
-                    let postResponse = yield promotions.post(promotion.options);
-                    expect(postResponse)
-                        .to.have.status(201)
-                        .to.have.schema(schemas.createPromotionResponse);
-                    let promotionId = postResponse.body.id;
-                    let getResponse = yield promotions.get(promotionId);
-                    let expectedJson = extend({}, promotion.options);
-                    expectedJson.technical_name += '';
-                    expect(getResponse)
-                        .to.have.status(200)
-                        .to.comprise.of.json(expectedJson)
-                        .to.have.schema(schemas.getPromotionResponse);
-                }));
+                it('should return correct response schema when {description}', function (promotion) {
+                    let postResponse = promotions.post(promotion.options);
+                    return expect(postResponse).to.be.a.createPromotionResponse;
+                });
             });
             dd(testData.incorrectPromotionsOpts, function () {
-                it('should not create promotion when {description}', function (promotion) {
+                it('should return error 400 when {description}', function (promotion) {
                     let postResponse = promotions.post(promotion.options);
-                    return expect(postResponse)
-                        .not.to.have.schema(schemas.createPromotionResponse)
-                        .not.to.have.status(201);
+                    return expect(postResponse).to.be.an.errorResponse(400);
                 });
             });
         });
-        describe('Delete', function () {
+        describe('Get promotion', function () {
+            let correctResponses = Array.from(testData.correctPromotionsOpts);
+            before('create some data', function () {
+                return co(function* () {
+                    for (let i = 0; i < correctResponses.length; i++) {
+                        let res = yield promotions.post(correctResponses[i].options);
+                        correctResponses[i].id = res.body.id;
+                    }
+                });
+            });
+            dd(correctResponses, function () {
+                it('should return correct response schema when {description}', function (promotion) {
+                    let getResponse = promotions.get(promotion.id);
+                    return expect(getResponse).to.be.a.getPromotionResponse;
+                });
+            });
+            dd(correctResponses, function () {
+                it('should return data that was posted when {description}', function (promotion) {
+                    let getResponse = promotions.get(promotion.id);
+                    let expectedJson = extend({}, promotion.options);
+                    expectedJson.technical_name += '';
+                    return expect(getResponse).to.comprise.of.json(expectedJson);
+                });
+            });
+        });
+        describe('Delete promotion', function () {
             it('should delete a promotion', co.wrap(function* () {
                 let postResponse = yield promotions.post(testData.correctPromotionsOpts[0]['options']);
                 let promotionId = postResponse.body.id;
                 let deleteResponse = yield promotions.delete(promotionId);
-                expect(deleteResponse).to.have.status(204);
+                expect(deleteResponse).to.be.a.deletePromotionResponse;
                 let getResponse = yield promotions.get(promotionId);
-                expect(getResponse).to.have.status(404);
+                expect(getResponse).to.be.an.errorResponse(404);
             }));
         });
-        describe('Get list', function () {
+        describe('Get list of promotions', function () {
             beforeEach('delete all promotions', function () {
                 return promotions.deleteAll();
             });
             dd([ {val:0},{val:1},{val:2} ], function () {
-                it('should return list with {val} item(s)', co.wrap(function* (count) {
+                it('should return correct response schema (list of {val} item(s))', co.wrap(function* (count) {
+                    let arrayOfOptions = testData.correctPromotionsOpts.slice(0, count.val).map( obj => obj.options );
+                    yield promotions.postAll(arrayOfOptions);
+                    let getAllResponse = yield promotions.getAll();
+                    expect(getAllResponse).to.be.a.getAllPromotionsResponse;
+                }));
+            });
+            dd([ {val:1},{val:2} ], function () {
+                it('should return data that was posted (list of {val} item(s))', co.wrap(function* (count) {
                     let arrayOfOptions = testData.correctPromotionsOpts.slice(0, count.val).map( obj => obj.options );
                     let expectedIds = yield promotions.postAll(arrayOfOptions);
                     let getAllResponse = yield promotions.getAll();
-                    expect(getAllResponse)
-                        .to.have.status(200)
-                        .to.have.schema(schemas.getAllPromotionsResponse);
                     expect(getAllResponse.body).to.have.length(count.val);
                     let actualIds = getAllResponse.body.map( obj => obj.id );
                     expect(actualIds.sort()).to.deep.equal(expectedIds.sort());
